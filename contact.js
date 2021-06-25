8850d9696ebb1d55d40adade7e299c16c86778fd
Yuvraj-Thakrar <75614135+Yuvraj-Thakrar@users.noreply.github.com>
2021-06-25 16:49:17 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,18 @@
+function home(){
+    window.location="index.html";
+}
+ 
+function about(){
+window.location="about.html";
+}
+
+function features(){
+    window.location="features.html";
+}
+
+function gallery(){
+    window.location="gallery.html";
+}
+function contact(){
+    window.location="contact.html";
+}
